e1700b3a85f3c85865c00d3ad2e4fb0f1464bfba
Michael Woo <michaelwoo36@gmail.com>
2023-01-12 21:31:09 -0800

    Updating Giant Branch

--- CritterMain.java
+++ CritterMain.java
@@ -17,8 +17,8 @@ public class CritterMain {
         // frame.add(30, Giant.class);
         // frame.add(30, Husky.class);
 
-        frame.add(30, FlyTrap.class);
-        frame.add(30, Food.class);
+        //frame.add(30, FlyTrap.class);
+        //frame.add(30, Food.class);
 
         frame.start();
     }
